dc592cfce24db02f4295d94827969c5e3c922e6f
Lemus <lemusangel1092@hotmail.com>
2021-12-13 16:01:16 -0600

    Agregue los liks siguiente y anterior, falta estilos hover

--- template-parts/single/content-single.php
+++ template-parts/single/content-single.php
@@ -1,3 +1,12 @@
+<?php
+        $prev_post = get_previous_post(); 
+        $id = $prev_post->ID ;
+        $permalink = get_permalink( $id );
+
+        $next_post = get_next_post();
+        $nid = $next_post->ID ;
+        $permalinkn = get_permalink($nid);
+    ?>
 <main id="content" <?php post_class('site-main'); ?>>
 
     <?php do_action('orbital_before_single_header'); ?>
@@ -12,20 +21,34 @@
             <div class="entry-post-content">
                 <?php the_content(); ?>
                  
-            <?php wp_link_pages(array('next_or_number' => 'next')); ?>
+                <?php wp_link_pages(array('next_or_number' => 'next')); ?>
 
-            <?php do_action('orbital_after_single_content'); ?>
-            <footer class="entry-footer">
+                <?php do_action('orbital_after_single_content'); ?>
+                <div class="next-prev-postlinks">
+                    
+                        <a href="<?php  echo $permalink;?>" class="prev-link">
+                            <i class="fas fa-angle-left"></i>
+                            <span><?php echo $prev_post->post_title;?></span>
+                        </a>
+                    
+                    
+                        <a href="<?php  echo $permalinkn;?>" class="next-link">
+                            <span><?php echo $next_post->post_title;?></span>
+                            <i class="fas fa-angle-right"></i>
+                        </a>
+                    
+                </div>
+                <footer class="entry-footer">
 
-                <?php do_action('orbital_before_single_comments'); ?>
+                    <?php do_action('orbital_before_single_comments'); ?>
 
-                <?php if (comments_open() || get_comments_number()) : ?>
-                    <?php comments_template(); ?>
-                <?php endif; ?>
+                    <?php if (comments_open() || get_comments_number()) : ?>
+                        <?php comments_template(); ?>
+                    <?php endif; ?>
 
-                <?php do_action('orbital_after_single_comments'); ?>
+                    <?php do_action('orbital_after_single_comments'); ?>
 
-            </footer>
+                </footer>
             </div>
            
 
